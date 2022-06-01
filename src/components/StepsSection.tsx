3e8543e7218a6de23b5c9f8171cee7528909cdcb
Thiago <trabalho.bandeira@gmail.com>
2022-06-01 19:14:03 -0300

    Add StepsSection Component

---
+++ src/components/StepsSection.tsx
@@ -0,0 +1,36 @@
+import StepsImage1 from '../assets/images/steps-image-1.png'
+import StepsImage2 from '../assets/images/steps-image-2.png'
+import StepsImage3 from '../assets/images/steps-image-3.png'
+
+function StepsSection() {
+  return (
+    <section className="steps-section">
+      <div className="steps-section__container">
+        <div className="steps">
+          <div className="steps-image">
+            <img src={StepsImage1} alt="" />
+          </div>
+          <h2>Order Your Food</h2>
+          <p>It takes only 10 seconds</p>
+        </div>
+        
+        <div className="steps">
+          <div className="steps-image">
+            <img src={StepsImage2} alt="" />
+          </div>
+          <h2>Delivery & Pickup</h2>
+          <p>It takes only 10 seconds</p>
+        </div>
+        <div className="steps">
+          <div className="steps-image">
+            <img src={StepsImage3} alt="" />
+          </div>
+          <h2>Delicious Recipe</h2>
+          <p>It takes only 10 seconds</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default StepsSection
