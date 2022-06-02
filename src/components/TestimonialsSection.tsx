ffef60d8f3db7ad5cd0179c3ab272014add7ea51
Thiago <trabalho.bandeira@gmail.com>
2022-06-02 17:19:52 -0300

    Add TestimonialsSection Component

---
+++ src/components/TestimonialsSection.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ButtonCTA from './ButtonCTA'
+
+function TestimonialsSection() {
+  return (
+    <section className="testimonials-section">
+      <div className="testimonials-section__intro">
+        <p>Costumers Testimonials</p>
+        <h2>What <span> Costumers </span>Say About Us</h2>
+        <p>We are happy when costumers are too</p>
+        <ButtonCTA text="Read more"/>
+      </div>
+      <div className="testimonials-section__cards">
+        
+      </div>
+    </section>
+  )
+}
+
+export default TestimonialsSection
