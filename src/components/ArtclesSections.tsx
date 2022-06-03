90e9633079532f2cfc48a2b7920026152dfc0f8d
Thiago <trabalho.bandeira@gmail.com>
2022-06-03 18:07:44 -0300

    Create Articles Section

---
+++ src/components/ArtclesSections.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Articles() {
+  return (
+    <div className="articles-section">
+      <p> <span> Articles </span> </p>
+      <h2>Recent <span> Posts</span> </h2>
+      <div className="article-section__container">
+        <article className="article-section__card">
+          
+
+        </article>
+      </div>
+    </div>
+  )
+}
