e85847ffaf2e57279b1aeca08e4f9a294c09b69d
Thiago <trabalho.bandeira@gmail.com>
2022-06-03 18:31:10 -0300

    Add ArticlesSection Component Style

--- src/components/ArtclesSections.tsx
+++ src/components/ArtclesSections.tsx
@@ -7,22 +7,22 @@ function Articles() {
       <p> <span> Articles </span> </p>
       <h2>Recent <span> Posts</span> </h2>
       <p>Take a look at our newest articles on how to eat better</p>
-      <div className="article-section__container">
-        <article className="article-section__card">
-          <div className="article-section__img">
+      <div className="articles-section__container">
+        <article className="articles-section__card">
+          <div className="articles-section__img">
             <img src={ImageArticle01} alt="" />
           </div>
-          <div className="article-section__content">
+          <div className="articles-section__content">
             <p>12 June 2022</p>
             <h3>Why isn't pizza boxed?</h3>
             <p>Why don't we box our pizza? It might seem odd at first. Almost all pizza business...</p>
             <ButtonCTA text="Read More"/>
           </div>
 
-          <div className="article-section__img">
+          <div className="articles-section__img">
             <img src={ImageArticle01} alt="" />
           </div>
-          <div className="article-section__content">
+          <div className="articles-section__content">
             <p>12 June 2022</p>
             <h3>Why isn't pizza boxed?</h3>
             <p>Why don't we box our pizza? It might seem odd at first. Almost all pizza business...</p>
