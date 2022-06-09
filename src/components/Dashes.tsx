a8d13fb573f06dd9780d25b6c62bae027f574423
Thiago <trabalho.bandeira@gmail.com>
2022-06-09 17:39:37 -0300

    Add Dashes component

---
+++ src/components/Dashes.tsx
@@ -0,0 +1,12 @@
+
+
+function Dashes() {
+  return (
+    <div className="steps-section__dashes">
+          <p className="dashes">|</p>
+          <p className="dashes">|</p>
+    </div>
+  )
+}
+
+export default Dashes
