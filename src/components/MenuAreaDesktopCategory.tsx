a9434c73020e9de07c01f331f9b7f9989940e6aa
Thiago <trabalho.bandeira@gmail.com>
2022-06-28 12:35:31 -0300

    add MenuAreaDesktopCategory component

--- src/components/MenuAreaDesktopCategory.tsx
+++ src/components/MenuAreaDesktopCategory.tsx
@@ -1,7 +1,13 @@
-function MenuAreaDesktopCategory() {
+interface Icon {
+  categoryIcon: string
+}
+
+function MenuAreaDesktopCategory({categoryIcon}:Icon) {
   return (
     <div>
-
+      <div className="category-icon-container">
+        <img src={categoryIcon} alt="" />
+      </div>
     </div>
   )
 }
